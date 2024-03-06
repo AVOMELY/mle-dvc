6ea6679714b0cff2fac9ceb29a1ca585b8a1e135
AVOMELY <avo@meavo.ru>
2024-03-06 17:26:29 +0000

    Изменение модели logisticreg

--- scripts/fit.py
+++ scripts/fit.py
@@ -1,3 +1,5 @@
+# scripts/fit.py
+
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
@@ -7,15 +9,29 @@ from catboost import CatBoostClassifier
 import yaml
 import os
 import joblib
-
+from sklearn.linear_model import LogisticRegression
 # обучение модели
 def fit_model():
   # Прочитайте файл с гиперпараметрами params.yaml
     with open('params.yaml', 'r') as fd:
         params = yaml.safe_load(fd)
     data = pd.read_csv('data/initial_data.csv')
+    
+    # print(data.isna().sum())
+    cols_with_nans = data.isnull().sum()
+    cols_with_nans = cols_with_nans[cols_with_nans > 0].index
+    print(cols_with_nans)
+    # data[cols_with_nans] = data[cols_with_nans].fillna('NO')
+    data[['end_date', 'total_charges', 'internet_service', 'online_security',
+       'online_backup', 'device_protection', 'tech_support', 'streaming_tv',
+       'streaming_movies', 'multiple_lines']]=data[['end_date', 'total_charges', 'internet_service', 'online_security',
+       'online_backup', 'device_protection', 'tech_support', 'streaming_tv',
+       'streaming_movies', 'multiple_lines']].fillna('NO')
+    # data = data.dropna()
     y=data['target']
-    print(data)
+    # print(y.value_counts())
+    # print(data.isna().sum())
+    # print('len',len(data))
   # загрузите результат предыдущего шага: inital_data.csv
     cat_features = data.select_dtypes(include='object')
     potential_binary_features = cat_features.nunique() == 2
@@ -27,15 +43,18 @@ def fit_model():
     preprocessor = ColumnTransformer(
         [
         ('binary', OneHotEncoder(drop='if_binary'), binary_cat_features.columns.tolist()),
-        ('cat', CatBoostEncoder(return_df=False), other_cat_features.columns.tolist()),
+        ('cat', OneHotEncoder(drop='if_binary'), binary_cat_features.columns.tolist()),
         ('num', StandardScaler(), num_features.columns.tolist())
         ],
         remainder='drop',
         verbose_feature_names_out=False
     )
 
-    model = CatBoostClassifier(auto_class_weights='Balanced')
-
+    model = LogisticRegression(
+        class_weight=params['class_weight'], 
+        C=params['reg_strength'], 
+        penalty=params['penalty']
+        )
     pipeline = Pipeline(
         [
             ('preprocessor', preprocessor),
@@ -52,4 +71,4 @@ def fit_model():
         joblib.dump(pipeline, fd) 
 
 if __name__ == '__main__':
-    fit_model()
+  fit_model()
